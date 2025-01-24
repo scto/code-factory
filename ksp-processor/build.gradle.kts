6c7f27a6fa4d2040ef9bd2ff2fb1aa6dd76d5024
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-01-24 19:05:37 +0300

    start

--- ksp-processor/build.gradle.kts
+++ ksp-processor/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.ksp)
     kotlin("jvm")
@@ -37,51 +39,91 @@ tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-            artifactId = project.name
-            version = project.version.toString()
-            groupId = project.group.toString()
+mavenPublishing {
+    coordinates(
+        groupId = project.group.toString(),
+        artifactId = project.name,
+        version = project.version.toString()
+    )
 
-            pom {
-                name.set("Code factory")
-                description.set("You wrote tests, then AI generates code.")
-                url.set("https://github.com/AntonButov/code-factory")
-                scm {
-                    url.set("https://github.com/AntonButov/code-factory")
-                }
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("antonbutov")
-                        name.set("Anton Butov")
-                        email.set("butov6101@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/AntonButov/code-factory.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:AntonButov/code-factory.git")
-                    url.set("https://github.com/AntonButov/code-factory")
-                }
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+    }
+
+    pom {
+        name.set("Code factory")
+        description.set("You wrote tests, then AI generates code.")
+        url.set("https://github.com/AntonButov/code-factory")
+        scm {
+            url.set("https://github.com/AntonButov/code-factory")
+        }
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("antonbutov")
+                name.set("Anton Butov")
+                email.set("butov6101@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/AntonButov/code-factory.git")
+            developerConnection.set("scm:git:ssh://git@github.com:AntonButov/code-factory.git")
+            url.set("https://github.com/AntonButov/code-factory")
         }
     }
 }
 
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("SIGNING_KEY"),
-        System.getenv("SIGNING_PASSWORD")
-    )
-    sign(publishing.publications["maven"])
-}
+//publishing {
+ //   publications {
+ //       create<MavenPublication>("maven") {
+  //          from(components["kotlin"])
+  //          artifactId = project.name
+  //          version = project.version.toString()
+  //          groupId = project.group.toString()
+//
+  //          pom {
+  //              name.set("Code factory")
+ //               description.set("You wrote tests, then AI generates code.")
+  //              url.set("https://github.com/AntonButov/code-factory")
+ //               scm {
+ //                   url.set("https://github.com/AntonButov/code-factory")
+ //               }
+ //               licenses {
+ //                   license {
+ //                       name.set("The Apache License, Version 2.0")
+ //                       url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+ //                   }
+ //               }
+ //               developers {
+ //                   developer {
+  //                      id.set("antonbutov")
+  //                      name.set("Anton Butov")
+ //                       email.set("butov6101@gmail.com")
+ //                   }
+ //               }
+ //               scm {
+ //                   connection.set("scm:git:git://github.com/AntonButov/code-factory.git")
+ //                   developerConnection.set("scm:git:ssh://git@github.com:AntonButov/code-factory.git")
+ //                   url.set("https://github.com/AntonButov/code-factory")
+ //               }
+ //           }
+ //       }
+ //   }
+//}
+
+//signing {
+//    useInMemoryPgpKeys(
+//        System.getenv("SIGNING_KEY"),
+//        System.getenv("SIGNING_PASSWORD")
+//    )
+//    sign(publishing.publications["maven"])
+//}
 
 
 tasks.test {
