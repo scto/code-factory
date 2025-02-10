cca551d2f796d4d0f0ff8e0f554ef687a41f07ec
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-02-10 21:07:22 +0300

    add lint

---
+++ code-factory-processor/src/main/kotlin/com/code/factory/coderesolver/CodeResolverImpl.kt
@@ -0,0 +1,24 @@
+package com.code.factory.coderesolver
+
+import com.google.devtools.ksp.symbol.KSDeclaration
+import java.nio.file.Files
+import kotlin.io.path.Path
+import kotlin.runCatching
+
+internal class CodeResolverImpl : CodeResolver {
+    @Throws(AssertionError::class)
+    override fun getCodeString(declaration: List<KSDeclaration>): List<String> {
+        assert(declaration.isNotEmpty())
+        return declaration.map {
+            it.containingFile?.filePath?.let {
+                fileCode(it)
+            } ?: "" // todo #46
+        }
+    }
+
+    private fun fileCode(fileName: String): String =
+        runCatching {
+            val path = Path(fileName)
+            Files.readString(path)
+        }.getOrNull() ?: error("Code not found.")
+}
