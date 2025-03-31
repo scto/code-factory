250bd4f4b3aa6c2739ef929289ebd8029afe0a5f
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-03-31 20:50:03 +0300

    Add logger to BasePathProvider and handle no files case.

--- code-factory-processor/src/main/kotlin/com/code/factory/ksp/BasePathProvider.kt
+++ code-factory-processor/src/main/kotlin/com/code/factory/ksp/BasePathProvider.kt
@@ -1,5 +1,6 @@
 package com.code.factory.ksp
 
+import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import jakarta.inject.Inject
 
@@ -9,9 +10,15 @@ interface BasePathProvider {
 
 class BasePathProviderImpl
     @Inject
-    constructor() : BasePathProvider {
+    constructor(
+        private val logger: KSPLogger,
+    ) : BasePathProvider {
         override fun getBasePath(resolver: Resolver): String? {
-            return resolver.getAllFiles().firstOrNull()?.filePath?.let {
+            val allFiles = resolver.getAllFiles()
+            if (allFiles.toList().isEmpty()) { // for diagnostic that case https://github.com/AntonButov/code-factory/issues/149
+                logger.warn("No files found")
+            }
+            return allFiles.firstOrNull()?.filePath?.let {
                 return it.split("src").first()
             }
         }
